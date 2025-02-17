24d24dd5dc1a2e12a6f2180dec4e2c50b527fceb
codegangpay <allhoodgang@gmail.com>
2025-02-17 21:01:04 +0900

    회원가입 중간 커밋

--- src/main/java/kr/co/iei/etc/controller/EtcController.java
+++ src/main/java/kr/co/iei/etc/controller/EtcController.java
@@ -2,6 +2,7 @@ package kr.co.iei.etc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,4 +16,13 @@ public class EtcController {
 	@Autowired
 	private MemberService memberService;
 	
+	@GetMapping(value="idCheck")
+	public int idCheck(Member m) {
+		Member member = memberService.selectOneMember(m);
+		int r = 0;
+		if(member != null) {
+			r = 1;
+		}
+		return r;
+	}
 }
