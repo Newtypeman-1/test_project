f538743830a45ae078155f62db75dc21a0630d8a
codegangpay <allhoodgang@gmail.com>
2025-02-17 10:23:36 +0900

    login commit

---
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package kr.co.iei.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.iei.member.model.service.MemberService;
+
+@Controller
+@RequestMapping(value="/member")
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping(value="/login")
+	public String login() {
+		return "member/login";
+	}
+}
