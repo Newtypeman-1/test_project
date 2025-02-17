d431403943eabc4467c52fb41f238256b78daee5
codegangpay <allhoodgang@gmail.com>
2025-02-17 13:05:45 +0900

    헤더푸터 커밋

--- src/main/java/kr/co/iei/etc/controller/EtcController.java
+++ src/main/java/kr/co/iei/etc/controller/EtcController.java
@@ -1,38 +1,24 @@
-package kr.co.iei.member.controller;
+package kr.co.iei.etc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import jakarta.servlet.http.HttpSession;
 import kr.co.iei.member.model.service.MemberService;
 import kr.co.iei.member.model.vo.Member;
 
 @Controller
-@RequestMapping(value="/member")
-public class MemberController {
+@RequestMapping(value="/etc")
+public class EtcController {
 	@Autowired
 	private MemberService memberService;
 	
-	@GetMapping(value="/loginFrm")
-	public String loginFrm() {
-		return "member/login";
-	}
-	
 	@ResponseBody
-	@PostMapping(value="/ajaxCheckId")
+	@PostMapping(value="ajaxCheckId")
 	public int ajaxCheckId(Member m) {
 		int r = memberService.checkLogin(m);
 		return r;
-	}
-	
-	@PostMapping(value="/login")
-	public String login(Member m, Model model, HttpSession session) {
-		
-		return "redirect:/";
 	}
 }
--- src/main/java/kr/co/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/co/iei/member/model/dao/MemberDao.java
@@ -1,9 +1,12 @@
 package kr.co.iei.member.model.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.co.iei.member.model.vo.Member;
 import kr.co.iei.member.model.vo.MemberRowMapper;
 
 @Repository
@@ -12,4 +15,15 @@ public class MemberDao {
 	private JdbcTemplate jdbc;
 	@Autowired
 	private MemberRowMapper memberRowMapper;
+	
+	public int checkLogin(Member m) {
+		String query = "select * from member_tbl where member_id = ? and member_pw = ?";
+		Object[] params = {m.getMemberId(), m.getMemberPw()};
+		List list = jdbc.query(query, memberRowMapper, params);
+		int r = 0;
+		if(!list.isEmpty()) {
+			r = 1;
+		}
+		return r;
+	}
 }
