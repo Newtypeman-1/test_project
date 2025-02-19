834e06e16b1eca3385203a4aa326ec89142f2268
codegangpay <allhoodgang@gmail.com>
2025-02-19 21:42:28 +0900

    112

--- src/main/java/kr/co/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/co/iei/member/model/dao/MemberDao.java
@@ -54,7 +54,7 @@ public class MemberDao {
 		String query = "select * from member_tbl order by 1";
 		List list = jdbc.query(query, memberRowMapper);
 		return list;
-
+	}
 	public int registerMember(Member m) {
 		String query = "insert into member_tbl values(member_seq.nextval, ?, ?, ?, ?, ?, ?, ?, to_char(sysdate,'yyyy-mm-dd'))";
 		Object[] params = {m.getMemberId(), m.getMemberPw(), m.getMemberName(), m.getMemberPhone(), m.getMemberAddr(), m.getMemberEmail(), m.getMemberGender()};
