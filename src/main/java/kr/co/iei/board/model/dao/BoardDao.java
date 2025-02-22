3b70dc0cbd5e94204b3fc61a04a8a1eafa9d5c4b
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-02-22 22:49:43 +0900

    보드 작업중

--- src/main/java/kr/co/iei/board/model/dao/BoardDao.java
+++ src/main/java/kr/co/iei/board/model/dao/BoardDao.java
@@ -1,5 +1,7 @@
 package kr.co.iei.board.model.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,17 @@ public class BoardDao {
 	private JdbcTemplate jdbc;
 	@Autowired
 	private BoardRowMapper boardRowMapper;
+	
+	public int selectBoardTotalCount() {
+		String query = "select count(*) from board";
+		return jdbc.queryForObject(query, Integer.class);
+	}
+
+	public List selectBoardList(int start, int end) {
+		String query = "select * from (select rownum as rnum, b.* from (select * from board order by 1 desc) b) where rnum between ? and ?";
+		Object[] params = {start, end};
+		List list = jdbc.query(query, boardRowMapper, params);
+		return list;
+	}
 
 }
--- src/main/java/kr/co/iei/board/model/service/BoardService.java
+++ src/main/java/kr/co/iei/board/model/service/BoardService.java
@@ -1,12 +1,25 @@
 package kr.co.iei.board.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.board.model.dao.BoardDao;
+import kr.co.iei.board.model.vo.Board;
 
 @Service
 public class BoardService {
 	@Autowired
 	private BoardDao boardDao;
+
+	public int selectBoardTotalCount() {
+		return boardDao.selectBoardTotalCount();
+	}
+
+	public List<Board> selectBoardList(int start, int amount) {
+		int end = start + amount - 1;
+		List list = boardDao.selectBoardList(start, end);
+		return list;
+	}
 }
