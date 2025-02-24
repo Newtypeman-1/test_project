95f37c385c6bcca45451348987276861e36c5464
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-02-24 13:43:55 +0900

    보드게시판 전면공사

--- src/main/java/kr/co/iei/board/model/dao/BoardDao.java
+++ src/main/java/kr/co/iei/board/model/dao/BoardDao.java
@@ -7,8 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import kr.co.iei.board.model.vo.BoardRowMapper;
-import kr.co.iei.doctor.model.vo.Doctor;
-import kr.co.iei.member.model.vo.Member;
 
 @Repository
 public class BoardDao {
@@ -17,31 +15,16 @@ public class BoardDao {
 	@Autowired
 	private BoardRowMapper boardRowMapper;
 	
-	public List selectBoardList(int start, int end) {
-		String query = "select * from (select rownum as rnum, b.* from (select * from board order by board_no desc)b) where rnum between ? and ?";
-		Object[] params = {start,end};
-		List list = jdbc.query(query, boardRowMapper,params);
-		return list;
-	}
-
 	public int selectBoardTotalCount() {
 		String query = "select count(*) from board";
-		int totalCount = jdbc.queryForObject(query, Integer.class);
-		return totalCount;
+		return jdbc.queryForObject(query, Integer.class);
 	}
 
-	public List allBoard(Doctor doctor) {
-		String query = "select * from (select rownum as rnum, h.* from(select r.*,(select doctor_name from doctor_tbl where doctor_no = r.doctor_no) doctor_name from comment_tbl r where doctor_no = ? order by 1 desc)h) where rnum between 1 and 5;";
-		Object[] params = {doctor.getDoctorNo()};
-		List allBoard = jdbc.query(query, boardRowMapper,params);
-		return allBoard;
-	}
-
-	public List memberAllBoard(Member member) {
-		String query = "select * from (select rownum as rnum, o.* from (select b.* from board b where board_writer = ? order by board_no desc)o) where rnum between 1 and 5";
-		Object[] params = {member.getMemberId()};
-		List memberAllBoard = jdbc.query(query, boardRowMapper, params);
-		return memberAllBoard;
+	public List selectBoardList(int start, int end) {
+		String query = "select * from (select rownum as rnum, b.* from (select * from board order by 1 desc) b) where rnum between ? and ?";
+		Object[] params = {start, end};
+		List list = jdbc.query(query, boardRowMapper, params);
+		return list;
 	}
 
 }
--- src/main/java/kr/co/iei/board/model/service/BoardService.java
+++ src/main/java/kr/co/iei/board/model/service/BoardService.java
@@ -6,34 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.board.model.dao.BoardDao;
-import kr.co.iei.doctor.model.vo.Doctor;
-import kr.co.iei.member.model.vo.Member;
+import kr.co.iei.board.model.vo.Board;
 
 @Service
 public class BoardService {
 	@Autowired
 	private BoardDao boardDao;
 
-	public List selectBoardList(int reqPage) {
-		int numPerPage = 10;
-		int end = reqPage * numPerPage;
-		int start = end - numPerPage + 1;
+	public int selectBoardTotalCount() {
+		return boardDao.selectBoardTotalCount();
+	}
+
+	public List<Board> selectBoardList(int start, int amount) {
+		int end = start + amount - 1;
 		List list = boardDao.selectBoardList(start, end);
-		int totalCount = boardDao.selectBoardTotalCount();
-		int totalPage = totalCount/numPerPage;
-		if(totalCount%numPerPage != 0) {
-			totalPage += 1;
-		}
 		return list;
-	}
-
-	public List allBoard(Doctor doctor) {
-		List allBoard = boardDao.allBoard(doctor);
-		return allBoard;
-	}
-
-	public List memberAllBoard(Member member) {
-		List memberAllBoard = boardDao.memberAllBoard(member);
-		return memberAllBoard;
 	}
 }
