f970ff0c95a1fdc40d0db9b4187d712a1e9feaa4
codegangpay <allhoodgang@gmail.com>
2025-02-25 20:53:32 +0900

    의사 문의기록 커밋

---
+++ src/main/java/kr/co/iei/board/model/vo/CommentRowMapper2.java
@@ -0,0 +1,24 @@
+package kr.co.iei.board.model.vo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentRowMapper2 implements RowMapper<Comment>{
+
+	@Override
+	public Comment mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Comment c = new Comment();
+		c.setBoardNo(rs.getInt("board_no"));
+		c.setCommentContent(rs.getString("comment_content"));
+		c.setCommentDate(rs.getString("comment_date"));
+		c.setCommentNo(rs.getInt("comment_no"));
+		c.setDoctorNo(rs.getInt("doctor_no"));
+		c.setIsDone(rs.getString("is_done"));
+		return c;
+	}
+
+}
